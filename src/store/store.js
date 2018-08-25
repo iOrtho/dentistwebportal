27786800b99ef1607dc21f418eaf637479f86800
FrenchMajesty <verdi.kap@gmail.com>
2018-08-25 10:29:50 -0500

    Revert "Add store's branch for `office`"
    This reverts commit 54fc471fe934a69db885703b901f37cf33a57d61.

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import user from './reducers/user';
-import office from './reducers/office';
 
 const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const reducer = combineReducers({
 	user,
-	office,
 });
 
 export default createStore(reducer, applyMiddleware(thunk), reduxDevTools);
