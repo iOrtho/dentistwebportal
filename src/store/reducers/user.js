9693c48b39b6f53e72074713dceed7d92fce0cf1
FrenchMajesty <verdi.kap@gmail.com>
2018-08-25 10:29:46 -0500

    Revert "Add user action: Toggle auth check status"
    This reverts commit bf94fa8eefbb9e11fe6374f79373ef6be1beb51a.

--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -2,17 +2,13 @@ import UserAction from '../actions/user';
 
 const initialState = {
 	id: null,
-	authCheckStatus: false,
 };
 
 export default function user(state = initialState, action) {
 	switch(action.type) {
 
 		case UserAction.SET_USER_MODEL:
-			return { ...state, ...action.data };
-
-		case UserAction.SET_AUTH_CHECK_STATUS:
-			return { ...state, authCheckStatus: true };
+			return {...state, ...action.data};
 
 		case UserAction.RESET_USER_MODEL:
 			return initialState;
