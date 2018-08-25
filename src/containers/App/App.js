054bc3e05fd3702542cba9b0901112662e5a51d1
FrenchMajesty <verdi.kap@gmail.com>
2018-08-25 10:29:45 -0500

    Revert "Update App container to set listeners for relevant data"
    This reverts commit 707e98ba529eb546850b12a0dc858a1b15cdbd9c.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,65 +1,9 @@
 import React, { Component } from 'react';
 import { Layout } from 'antd';
 import Navbar from 'components/Navbar';
-import { connect } from 'react-redux';
-import { auth, database } from 'config/firebase';
-import UserAction from 'store/actions/user';
-import OfficeAction from 'store/actions/office';
 import './style.css';
 
 class App extends Component {
-
-	/**
-	 * Set listeners for all relevant data
-	 */
-	componentDidMount() {
-		this.loadOfficeDetails();
-		this.loadUserDetails();
-	}
-
-	/**
-     * Set a listener on the office model and sync it to the store
-     * @return {Void} 
-     */
-    loadOfficeDetails() {
-        const {id} = this.props.office;
-        const Offices = database.collection('Offices');
-
-        Offices.doc(id).onSnapshot(doc => {
-        	this.props.setOfficeModel({
-                ...doc.data(),
-                ready: true,
-            });
-        });
-    }
-
-    /**
-     * Set a listener on the user model and sync it to the store
-     * @return {Void} 
-     */
-    loadUserDetails() {
-    	auth.onAuthStateChanged((user) => {
-            if(user) {
-            	database.collection('Users').where('auth_id', '==', user.uid)
-            	.onSnapshot(snap => {
-                    snap.docChanges().forEach(change => {
-                        const data = {
-                            ...change.doc.data(),
-                            id: change.doc.id,
-                            authCheckStatus: true,
-                        };
-
-                        this.props.resetUserModel();
-                        this.props.setUserModel(data);
-                    });
-                });
-
-            }else {
-                this.props.resetUserModel();
-                this.props.setAuthCheckStatus(true);
-            }
-        });
-    }
 
 	/**
 	 * Render the component's markup
@@ -74,28 +18,4 @@ class App extends Component {
 	}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-/**
- * Map the redux store's state to the component's props
- * @param  {Object} options.office The office model
- * @return {Object}                  
- */
-function mapStateToProps({office}) {
-	return {
-		office,
-	};
-}
-
-/**
- * Map the actions and dispatch of the store to the component's props
- * @param  {Function} dispatch The store's action dispatcher
- * @return {Object}          
- */
-function mapDispatchToProps(dispatch) {
-	return {
-		setUserModel: (data) => dispatch(UserAction.setUserModel(data)),
-		setAuthCheckStatus: () => dispatch(UserAction.setAuthCheckStatus()),
-		setOfficeModel: (data) => dispatch(OfficeAction.setOfficeModel(data)),
-	};
-}
+export default App;
