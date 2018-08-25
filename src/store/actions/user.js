abb0ffacff119416db8456f59d0d3039a2efe5d3
FrenchMajesty <verdi.kap@gmail.com>
2018-08-25 10:45:02 -0500

    Add UserAction: `RESET_USER_MODEL` and `SET_AUTH_CHECK_STATUS`

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -1,6 +1,8 @@
 export default class User {
 
 	static SET_USER_MODEL = 'SET_USER_MODEL';
+	static RESET_USER_MODEL = 'RESET_USER_MODEL';
+	static SET_AUTH_CHECK_STATUS = 'SET_AUTH_CHECK_STATUS';
 	static UPDATE_OFFICE_MODEL = 'UPDATE_OFFICE_MODEL';
 
 	/**
@@ -12,6 +14,26 @@ export default class User {
 		return {
 			type: this.SET_USER_MODEL,
 			data,
+		};
+	}
+
+	/**
+	 * Return the action to set the user's auth check status to true
+	 * @return {Object} 
+	 */
+	static setAuthCheckStatus() {
+		return {
+			type: this.SET_AUTH_CHECK_STATUS,
+		};
+	}
+
+	/**
+	 * Return the action to reset the user's model
+	 * @return {Object} 
+	 */
+	static restUserModel() {
+		return {
+			type: this.RESET_USER_MODEL,
 		};
 	}
 
--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -2,13 +2,20 @@ import UserAction from '../actions/user';
 
 const initialState = {
 	id: null,
+	authCheckStatus: false,
 };
 
 export default function user(state = initialState, action) {
 	switch(action.type) {
 
 		case UserAction.SET_USER_MODEL:
-			return {...state, ...action.data};
+			return { ...state, ...action.data };
+
+		case UserAction.SET_AUTH_CHECK_STATUS:
+			return { ...state, authCheckStatus: true };
+
+		case UserAction.RESET_USER_MODEL:
+			return initialState;
 
 		case UserAction.UPDATE_OFFICE_MODEL:
 			return {
