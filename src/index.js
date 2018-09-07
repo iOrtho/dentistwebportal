525c000fed3555e2070fc79465642a05a7566016
FrenchMajesty <verdi.kap@gmail.com>
2018-09-07 11:19:39 -0500

    #7 : Add `bugsnag` to catch bugs and errors in the web app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import createPlugin from 'bugsnag-react';
+import bugsnag from 'bugsnag-js';
 import ReactDOM from 'react-dom';
 import Router from './router';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import registerServiceWorker from './registerServiceWorker';
 
+const {REACT_APP_BUGSNAG_KEY: BUGSNAG_KEY} = process.env;
+const bugsnagClient = bugsnag(BUGSNAG_KEY);
+
+
+const ErrorBoundary = bugsnagClient.use(createPlugin(React));
 const app = (
-	<Provider store={store}> 
-		<Router />
-	</Provider>
+    <ErrorBoundary>
+        <Provider store={store}> 
+            <Router />
+        </Provider>
+    </ErrorBoundary>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
+
+bugsnagClient.notify(new Error('Test error'))
