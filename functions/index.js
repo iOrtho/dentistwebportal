19c6a138f12e2584b5e2ae174291f0f6fdf43976
FrenchMajesty <verdi.kap@gmail.com>
2018-11-06 10:58:09 -0600

    Update and improve Firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,42 @@
-const functions = require('firebase-functions');
+const Functions = require('firebase-functions');
+const Admin = require('firebase-admin');
+const cors = require('cors')({origin: true});
+const { Expo } = require('expo-server-sdk');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const config = {
+    apiKey: 'AIzaSyCSlKkhihmDZy9nJbFe7hBKlFR9i_RMVwk',
+    authDomain: 'doctorapp-9c43b.firebaseapp.com',
+    databaseURL: 'https://doctorapp-9c43b.firebaseio.com',
+    projectId: 'doctorapp-9c43b',
+    storageBucket: 'gs://doctorapp-9c43b.appspot.com',
+    messagingSenderId: '249046440701',
+};
+
+Admin.initializeApp(config);
+
+exports.sendNotification = Functions.https.onRequest((req, res) => {
+    cors(req, res, () => {});
+    
+    const {expoTokenId} = req.body;
+    const ExpoSDK = new Expo();
+
+    if (!Expo.isExpoPushToken(expoTokenId)) {
+        return res.status(400).json({
+            success: false,
+            message: 'The user\'s device ID is not valid.',
+        });
+    }
+
+    const message = [{
+        to: expoTokenId,
+        sound: 'default',
+        body: 'You have received a new message!',
+        data: { target: 'chat' },
+    }];
+
+    return ExpoSDK.sendPushNotificationsAsync(message)
+            .then(data => {
+                return res.json({success: true});
+            })
+            .catch(console.error);
+});
